15a3720fdb56a6ff572f1dd345cde80e4b5f56b4
Lezt Waper <leztwaper@gmail.com>
2025-07-20 09:14:33 +0700

    Stop using services of port 5555, and use the default option

--- app/Http/Controllers/clients/HomeController.php
+++ app/Http/Controllers/clients/HomeController.php
@@ -27,37 +27,7 @@ class HomeController extends Controller
         $tours = $this->homeTours->getHomeTours();
 
         $userId = $this->getUserId();
-        if ($userId) {
-            
-            // Gọi API Python để lấy danh sách tour được gợi ý cho từng người dùng 
-            try {
-                $apiUrl = 'http://127.0.0.1:5555/api/user-recommendations';
-                $response = Http::get($apiUrl, [
-                    'user_id' => $userId
-                ]);
-
-                if ($response->successful()) {
-                    $tourIds = $response->json('recommended_tours');
-                } else {
-                    $tourIds = [];
-                }
-            } catch (\Exception $e) {
-                // Xử lý lỗi khi gọi API
-                $tourIds = [];
-                \Log::error('Lỗi khi gọi API liên quan: ' . $e->getMessage());
-            }
-
-            $toursPopular = $this->tours->toursRecommendation($tourIds);
-
-            if (empty($tourIds)) {
-                $toursPopular = $this->tours->toursPopular(6);
-                
-            }
-
-            // dd($toursPopular);
-        }else {
-            $toursPopular = $this->tours->toursPopular(6);
-        }
+        $toursPopular = $this->tours->toursPopular(6);
 
         // dd($toursPopular);
         return view('clients.home', compact('title', 'tours', 'toursPopular'));
--- app/Http/Controllers/clients/MyTourController.php
+++ app/Http/Controllers/clients/MyTourController.php
@@ -24,33 +24,7 @@ class MyTourController extends Controller
         $userId = $this->getUserId();
         
         $myTours = $this->user->getMyTours($userId);
-        $userId = $this->getUserId();
-        if ($userId) {
-            // Gọi API Python để lấy danh sách tour được gợi ý cho từng người dùng 
-            try {
-                $apiUrl = 'http://127.0.0.1:5555/api/user-recommendations';
-                $response = Http::get($apiUrl, [
-                    'user_id' => $userId
-                ]);
-
-                if ($response->successful()) {
-                    $tourIds = $response->json('recommended_tours');
-                    $tourIds = array_slice($tourIds, 0, 2);
-                } else {
-                    $tourIds = [];
-                }
-            } catch (\Exception $e) {
-                // Xử lý lỗi khi gọi API
-                $tourIds = [];
-                Log::error('Lỗi khi gọi API liên quan: ' . $e->getMessage());
-            }
-
-
-            $toursPopular = $this->tours->toursRecommendation($tourIds);
-            // dd($toursPopular);
-        }else {
-            $toursPopular = $this->tours->toursPopular(6);
-        }
+        $toursPopular = $this->tours->toursPopular(6);
 
         return view('clients.my-tours', compact('title', 'myTours','toursPopular'));
     }
